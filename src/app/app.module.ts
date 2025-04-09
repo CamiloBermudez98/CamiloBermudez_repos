5a22c98f56c7ddc704d9756ac1e48cfdb006400c
CamiloBermudez98 <camilobermudez98@hotmail.com>
2025-04-09 11:12:31 -0500

    add dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,34 @@ import { SpecificPokemonComponent } from './componentes/specific-pokemon/specifi
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FilterByNamePipe } from './componentes/pipe/filterByName';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { CreateObjetComponent } from './componentes/main-page/create-objet/create-objet.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    SpecificPokemonComponent,FilterByNamePipe
+    SpecificPokemonComponent,FilterByNamePipe,CreateObjetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     HttpClientModule,
     MatProgressSpinnerModule,
-    FormsModule 
+    FormsModule ,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
